0025166104031a492df328844b5ab163a32a173c
Faizan Ali <faizanali04@hotmail.com>
2018-02-07 20:50:02 -0800

    feat: Add remaining methods minus open position chore: Refactor lendroid.ts web3 calls

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,4 +1,4 @@
 export enum Token {
-    OMG = 'OMG',
-    ETH = 'ETH'
+    OMG = '0xcc2704ce33089d0f051eb0aff1750bb99fdfab46',
+    ETH = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
 }
--- src/services/web3-service.ts
+++ src/services/web3-service.ts
@@ -1,15 +1,18 @@
 import * as t from 'web3/types'
 import Web3 = require("web3")
 import { Context, Logger } from './logger'
+import { Contract } from 'web3/types'
+import { getWalletAbi, walletAddress } from '../constants/deployed-constants'
 
 export class Web3Service {
 
-    // @ts-ignore
     private web3
+    // @ts-ignore
+    private walletContract: Contract
 
     constructor(provider: t.Provider | string) {
         if (!provider || (typeof provider === 'string' && !(provider.includes('localhost:') || provider.includes('127.0.0.1:')))) {
-            Logger.info(Context.WEBPACK, `message=Invalid provider, provider=${provider}`)
+            Logger.info(Context.WEBPACK, `message=Invalid Web3 provider, provider=${provider}`)
             throw new Error('Invalid Web3 provider')
         } else {
             // @ts-ignore
@@ -23,5 +26,32 @@ export class Web3Service {
             Logger.error(Context.WEB3, 'message=Web3 not defined')
         }
         return this.web3
+    }
+
+    /**
+     * Obtains the user's account address
+     */
+    public async userAccount(): Promise<string> {
+        try {
+            return (await this.web3.eth.getAccounts())[0]
+        } catch (error) {
+            Logger.error(Context.WEB3, `message=Error retrieving user account, error=${error}`)
+            throw Error(error)
+        }
+    }
+
+    /**
+     * Initializes the wallet contract if undefined and returns it
+     */
+    public async getWalletContract(): Promise<Contract> {
+        if (!this.walletContract) {
+            try {
+                this.walletContract = new this.web3.eth.Contract(await getWalletAbi(), walletAddress)
+            } catch (error) {
+                Logger.error(Context.WEB3, `message=Error retrieving user account, error=${error}`)
+                throw Error(error)
+            }
+        }
+       return this.walletContract
     }
 }
