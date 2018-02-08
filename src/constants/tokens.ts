85ba8102b7a738aa890a83065879e6221d3f2df2
Faizan Ali <faizanali04@hotmail.com>
2018-02-07 23:22:16 -0800

    feat: GET and POST loan offers

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,4 +1,9 @@
-export enum Token {
+export enum TokenName {
+    OMG = 'OMG',
+    ETH = 'ETH'
+}
+
+export enum TokenAddress {
     OMG = '0xcc2704ce33089d0f051eb0aff1750bb99fdfab46',
     ETH = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
 }
