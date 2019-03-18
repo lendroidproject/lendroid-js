74856902e1fef392d00b1e1da99bc960cd2b1d5c
alonavns <alona.vns@outlook.com>
2019-03-18 07:35:01 +0200

    catch api

--- src/constants/apis.ts
+++ src/constants/apis.ts
@@ -3,26 +3,38 @@ import axios from 'axios'
 export const apiGet = (base, endPoint, cb) => {
   const url = base + endPoint
 
-  axios.get(url)
+  axios
+    .get(url)
     .then(res => {
       cb(null, res.data)
+    })
+    .catch(err => {
+      cb(err)
     })
 }
 
 export const apiPost = (base, endPoint, data, cb) => {
   const url = base + endPoint
 
-  axios.post(url, data)
+  axios
+    .post(url, data)
     .then(res => {
       cb(null, res.data)
+    })
+    .catch(err => {
+      cb(err)
     })
 }
 
 export const apiDelete = (base, endPoint, cb) => {
   const url = base + endPoint
 
-  axios.delete(url)
+  axios
+    .delete(url)
     .then(res => {
       cb(null, res)
+    })
+    .catch(err => {
+      cb(err)
     })
 }
