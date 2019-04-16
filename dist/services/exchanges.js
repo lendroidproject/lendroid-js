ba932f0a9e691f7f1052a48bd4c2655ea7fa37ce
alonavns <alona.vns@outlook.com>
2019-04-16 17:35:52 +0300

    exchange rate

--- dist/services/exchanges.js
+++ dist/services/exchanges.js
@@ -2,11 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 exports.getTokenExchangeRate = function (token, callback) {
-    var tick = token.toLowerCase();
-    if (token === 'USDC') {
-        tick = 'usd-coin';
-    }
-    var url = "https://api.coinmarketcap.com/v1/ticker/" + tick + "//?convert=ETH";
+    var url = "https://min-api.cryptocompare.com/data/price?fsym=" + token + "&tsyms=ETH";
     axios_1.default.get(url)
         .then(function (res) {
         var result = res.data[0];
--- src/services/exchanges.ts
+++ src/services/exchanges.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export const getTokenExchangeRate = (token, callback) => {
-  let tick = token.toLowerCase()
-  if (token === 'USDC') { tick = 'usd-coin' }
-  const url = `https://api.coinmarketcap.com/v1/ticker/${tick}//?convert=ETH`
+  // let tick = token.toLowerCase()
+  // if (token === 'USDC') { tick = 'usd-coin' }
+  // const url = `https://api.coinmarketcap.com/v1/ticker/${tick}//?convert=ETH`
+  const url = `https://min-api.cryptocompare.com/data/price?fsym=${token}&tsyms=ETH`
   axios.get(url)
     .then(res => {
       const result = res.data[0]
