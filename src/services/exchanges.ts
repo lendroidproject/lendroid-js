06de500c07d4b79e4aca3cfa3f4cf5f3fc6950bf
alonavns <alona.vns@outlook.com>
2019-06-15 05:53:35 +0300

    comments

--- src/services/exchanges.ts
+++ src/services/exchanges.ts
@@ -1,18 +1,13 @@
 import axios from 'axios'
 
 export const getTokenExchangeRate = (token, callback) => {
-  // let tick = token.toLowerCase()
-  // if (token === 'USDC') { tick = 'usd-coin' }
-  // const url = `https://api.coinmarketcap.com/v1/ticker/${tick}//?convert=ETH`
   const url = `https://min-api.cryptocompare.com/data/price?fsym=${token}&tsyms=ETH`
   axios.get(url)
     .then(res => {
       const result = res.data.ETH
       callback(1 / result)
-      // setTimeout(getTokenExchangeRate, 12 * 1000, token, callback)
     })
     .catch(err => {
       callback(1)
-      // setTimeout(getTokenExchangeRate, 12 * 1000, token, callback)
     })
 }
